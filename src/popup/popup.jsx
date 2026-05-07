9a1bfddc0b79095080dc5abdad7eb2966ec0b476
radiant-radix <radxploit@gmail.com>
2026-05-07 11:38:30 +0700

    fix: LockScreen import path in popup.jsx

--- src/popup/popup.jsx
+++ src/popup/popup.jsx
@@ -8,7 +8,7 @@ import Welcome from './pages/Welcome'
 import CreateWallet from './pages/CreateWallet'
 import ImportWallet from './pages/ImportWallet'
 import Dashboard from './pages/Dashboard'
-import LockScreen from '../../components/wallet/LockScreen'
+import LockScreen from '../components/wallet/LockScreen'
 import DappConnect from './pages/DappConnect'
 import DappSign from './pages/DappSign'
 
